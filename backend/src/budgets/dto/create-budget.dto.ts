8a6df9b6e01685aafd01dc5e9d1e9a37b0ebc142
Osmario-filho <osmarioslfilho@gmail.com>
2025-10-02 13:08:53 -0300

    criação dos DTOs

--- backend/src/budgets/dto/create-budget.dto.ts
+++ backend/src/budgets/dto/create-budget.dto.ts
@@ -1,1 +1,22 @@
-export class CreateBudgetDto {}
+import { IsNotEmpty, IsNumber, IsInt, Min, Max, IsUUID } from 'class-validator';
+
+export class CreateBudgetDto {
+  @IsNotEmpty()
+  @IsNumber()
+  amount: number;
+
+  @IsNotEmpty()
+  @IsInt()
+  @Min(1)
+  @Max(12)
+  month: number;
+
+  @IsNotEmpty()
+  @IsInt()
+  @Min(2000)
+  year: number;
+
+  @IsNotEmpty({ message: 'O orçamento deve pertencer a uma categoria.' })
+  @IsUUID()
+  categoryId: string;
+}
--- backend/src/transactions/dto/create-transaction.dto.ts
+++ backend/src/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,31 @@
-export class CreateTransactionDto {}
+import {
+  IsNotEmpty,
+  IsString,
+  IsNumber,
+  IsDateString,
+  IsEnum,
+  IsUUID,
+} from 'class-validator';
+import { TransactionType } from '../entities/transaction.entity';
+
+export class CreateTransactionDto {
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  amount: number;
+
+  @IsNotEmpty()
+  @IsDateString()
+  date: string;
+
+  @IsNotEmpty()
+  @IsEnum(TransactionType)
+  type: TransactionType;
+
+  @IsNotEmpty({ message: 'A transação deve pertencer a uma categoria.' })
+  @IsUUID()
+  categoryId: string;
+}
