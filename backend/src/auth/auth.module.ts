e433e1c2b45a2c85b6113a345771e8aaa1db2958
Osmario-filho <osmarioslfilho@gmail.com>
2025-10-09 13:35:56 -0300

    continuando a lógica de criação de jwt token

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -5,10 +5,17 @@ import { HashingServiceProtocol } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/database/entities';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from './config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Global()
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+  ],
   controllers: [AuthController],
   providers: [
     AuthService,
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,9 +1,12 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, Inject } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { Repository } from 'typeorm';
 import { User } from 'src/database/entities';
 import { InjectRepository } from '@nestjs/typeorm';
 import { HashingServiceProtocol } from './hashing/hashing.service';
+import jwtConfig from './config/jwt.config';
+import type { ConfigType } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
@@ -11,6 +14,9 @@ export class AuthService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
     private readonly hashingServiceProtocol: HashingServiceProtocol,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+    private readonly jwtService: JwtService,
   ) {}
 
   async login(LoginDto: LoginDto) {
@@ -32,9 +38,24 @@ export class AuthService {
       throwError = false;
     }
 
-    if (throwError) {
+    if (throwError || !user) {
       throw new UnauthorizedException('Usuário ou senha inválidos');
     }
-    return LoginDto;
+
+    const accessToken = await this.jwtService.signAsync(
+      {
+        sub: user.id,
+      },
+      {
+        audience: this.jwtConfiguration.audience,
+        issuer: this.jwtConfiguration.issuer,
+        secret: this.jwtConfiguration.secret,
+        expiresIn: this.jwtConfiguration.jwtTtl,
+      },
+    );
+
+    return {
+      accessToken,
+    };
   }
 }
